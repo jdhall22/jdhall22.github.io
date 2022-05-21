1414b83c10840ec9b04105c6e27b7411fbaa3d52
Justin <hal11015@byui.edu>
2022-05-20 22:33:18 -0600

    js2

--- Assignments/Weather/js/openWeather.js
+++ Assignments/Weather/js/openWeather.js
@@ -13,13 +13,27 @@ fetch(requestURL)
   });
 
   function displayWeather(weather) {
+      let card = document.createElement("Section");
       let temp = document.createElement("p");
       let high = document.createElement("p");
       let wind = document.createElement("p");
       let humid = document.createElement("p");
       let speed = document.createElement("p");
 
-      weather.innerHTML = '<strong>Current: </strong> ${weather.temp}<br> <strong>High: </strong> ${weather.temp_max}<br> <strong>Wind Chill: </strong> ${weather.feels_like}<br> <strong>Humidity: </strong> ${weather.humidity}<br> <strong>Wind Speed: </strong> ${weather.speed}';
+      temp.textContent = `${weather.temp}`;
+      high.textContent = `${weather.temp_max}`;
+      wind.textContent = `${weather.feels_like}`;
+      humid.textContent = `${weather.humidity}`;
+      speed.textContent = `${wind.speed}`;
 
-    
+      weather.innerHTML = `<strong>Current: </strong> ${weather.temp}<br> <strong>High: </strong> ${weather.temp_max}<br> <strong>Wind Chill: </strong> ${weather.feels_like}<br> <strong>Humidity: </strong> ${weather.humidity}<br> <strong>Wind Speed: </strong> ${weather.speed}`;
+
+
+    card.appendChild(temp);
+    card.appendChild(high);
+    card.appendChild(wind);
+    card.appendChild(humid);
+    card.appendChild(speed);
+
+    block.appendChild(card);
   }
