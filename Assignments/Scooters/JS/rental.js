b982afe0808df4b8f6c45e23de25e67b0870720f
Justin <hal11015@byui.edu>
2022-05-30 11:21:08 -0500

    data

--- Assignments/Scooters/JS/rental.js
+++ Assignments/Scooters/JS/rental.js
@@ -13,17 +13,28 @@ fetch(eventsURL)
 
 function displayInfo(tours) {
   
-  if (tours.name == "Fish Haven") {
     let card = document.createElement("section");
-    let event = document.createElement("p");
-    //let town = document.createElement("p");
-    event.textContent = `${tours.events}`;
-  
-    card.appendChild(event);
-    data.appendChild(card);
-  }
+    let name = document.createElement("h3");
+    let persons = document.createElement("p");
+    let reservationHalfDay = document.createElement("p");
+    let reservationFullDay = document.createElement("p");
+    let walkHalfDay = document.createElement("p");
+    let walkFullDay = document.createElement("p");
 
-  else {
-    
-  }
+    name.textContent = `${tours.name}`;
+    persons.textContent = `${tours.persons}`;
+    reservationHalfDay.textContent = `${tours.reservationHalfDay}`;
+    reservationFullDay.textContent = `${tours.reservationFullDay}`;
+    walkHalfDay.textContent = `${tours.walkHalfDay}`;
+    walkFullDay.textContent = `${tours.walkFullDay}`;
+  
+    card.appendChild(name);
+    card.appendChild(persons);
+    card.appendChild(reservationHalfDay);
+    card.appendChild(reservationFullDay);
+    card.appendChild(walkHalfDay);
+    card.appendChild(walkFullDay);
+
+    data.appendChild(card);
+
   }
