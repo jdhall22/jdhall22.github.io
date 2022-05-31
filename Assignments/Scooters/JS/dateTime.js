9f2f57103b24291e0a37997132360af5efff7ea2
Justin <hal11015@byui.edu>
2022-05-31 17:10:50 -0500

    fix

--- Assignments/Scooters/JS/dateTime.js
+++ Assignments/Scooters/JS/dateTime.js
@@ -9,28 +9,6 @@ function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
-/**Friday pancake banner*/
-    var now  = new Date();       
-	var banner = document.querySelector("#fridayBanner")
-	
-	var weekday = new Array(7);
-        weekday[0] = "Sunday";
-        weekday[1] = "Monday";
-        weekday[2] = "Tuesday";
-        weekday[3] = "Wednesday";
-        weekday[4] = "Thursday";
-        weekday[5] = "Friday";
-        weekday[6] = "Saturday";
-
-    var day = weekday[now.getDay()];
-
-
-    if (day == "Friday") {
-        banner.style.display = "inline";
-    }
-    else {
-        banner.style.display = "none";
-      }
 
 
 /**5 day forcast */
